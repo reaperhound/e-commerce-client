8406d30eae6dabb1e893507f2522a0d8dcceb079
reaperhound <reaperhound69004@gmail.com>
2023-08-03 23:20:49 +0530

    initiated stripe

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <Elements>
+  <Elements stripe={stripePromise}>
     <App />
   </Elements>
   // </React.StrictMode>,
--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -1,3 +1,3 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-export const stripePromise = loadStripe("pk_live_51MsNKoSFKWLz2U01IT65LndRJzeZ1A1CQLS0XOnQylI2dbJso38sKnfzCUWjkyfCNu67brEKXj5fa3rSjTAG3fJ200VRMnrBvX")
+export const stripePromise = loadStripe("pk_live_51MsNKoSFKWLz2U01IT65LndRJzeZ1A1CQLS0XOnQylI2dbJso38sKnfzCUWjkyfCNu67brEKXj5fa3rSjTAG3fJ200VRMnrBvX") 
